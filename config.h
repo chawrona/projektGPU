bd437379f78921fd5deff96996eda8ec867036ef
chawrona <dawidchawrona@gmail.com>
2026-05-03 13:59:08 +0200

    asd

--- config.h
+++ config.h
@@ -5,9 +5,9 @@ constexpr float        SPEED_K      = 0.2f;
 constexpr float        ENERGY = 1200.0f;
 
 constexpr int          BLOCK_SIZE = 16;
-constexpr int          MATRIX_SIZE = BLOCK_SIZE * 6;
+constexpr int          MATRIX_SIZE = BLOCK_SIZE * 6 * 10;
 
-constexpr int          STEPS_LENGTH = 5;
-constexpr int          STEPS[] = {100, 500, 2000, 5000, 10000};
+constexpr int          STEPS_LENGTH = 7;
+constexpr int          STEPS[] = {100, 500, 1000, 2500, 5000, 7500, 10000};
 constexpr int          TEST_COUNT = 5;
 
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all:
-	nvcc main.cu test_cpu.cu test_gpu.cu utils.cu -o wave_sim -gencode arch=compute_50,code=compute_50
+	nvcc main.cu test_cpu.cu test_gpu.cu utils.cu -o simulation -gencode arch=compute_50,code=compute_50
